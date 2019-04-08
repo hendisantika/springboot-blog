a221802a496d7b52828e67612753ab98400617bf
hendisantika <hendisantika@yahoo.co.id>
2019-04-08 21:12:13 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/sprinbootblog/SprinbootBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.sprinbootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbootBlogApplication.class, args);
+    }
+
+}
