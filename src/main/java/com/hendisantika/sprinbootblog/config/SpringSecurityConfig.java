8feeb86875aeaf51a16f63c3e224a6d73a518327
hendisantika <hendisantika@yahoo.co.id>
2019-04-12 05:28:36 +0700

    #34 - Add configure method in SpringSecurityConfig

--- src/main/java/com/hendisantika/sprinbootblog/config/SpringSecurityConfig.java
+++ src/main/java/com/hendisantika/sprinbootblog/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.hendisantika.sprinbootblog.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -37,5 +38,35 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     public SpringSecurityConfig(AccessDeniedHandler accessDeniedHandler, DataSource dataSource) {
         this.accessDeniedHandler = accessDeniedHandler;
         this.dataSource = dataSource;
+    }
+
+    /**
+     * HTTPSecurity configurer
+     * - roles ADMIN allow to access /admin/**
+     * - roles USER allow to access /user/** and /newPost/**
+     * - anybody can visit /, /home, /registration, /error, /blog/**, /post/**, /h2-console/**
+     * - every other page needs authentication
+     * - custom 403 access denied handler
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/home", "/registration", "/error", "/blog/**", "/post/**", "/h2-console/**").permitAll()
+                .antMatchers("/newPost/**", "/commentPost/**", "/createComment/**").hasAnyRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/home")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+                // Fix for H2 console
+                .and().headers().frameOptions().disable();
     }
 }
