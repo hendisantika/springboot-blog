d651158b461721fb922aaf5edbf88bac2f8ef57e
hendisantika <hendisantika@yahoo.co.id>
2019-04-12 05:28:13 +0700

    #33 - Add SpringSecurityConfig

---
+++ src/main/java/com/hendisantika/sprinbootblog/config/SpringSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.hendisantika.sprinbootblog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sprinboot-blog
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-12
+ * Time: 05:27
+ */
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    final DataSource dataSource;
+    private final AccessDeniedHandler accessDeniedHandler;
+    @Value("${spring.admin.username}")
+    private String adminUsername;
+
+    @Value("${spring.admin.username}")
+    private String adminPassword;
+
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
+
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
+
+    @Autowired
+    public SpringSecurityConfig(AccessDeniedHandler accessDeniedHandler, DataSource dataSource) {
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.dataSource = dataSource;
+    }
+}
