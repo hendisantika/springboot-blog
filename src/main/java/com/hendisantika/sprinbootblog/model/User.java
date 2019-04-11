382a173d4b842d8177c2614bb72a81ee30d1dc3a
hendisantika <hendisantika@yahoo.co.id>
2019-04-11 21:31:04 +0700

    #22 - Modify User Model

--- src/main/java/com/hendisantika/sprinbootblog/model/User.java
+++ src/main/java/com/hendisantika/sprinbootblog/model/User.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.util.Collection;
 
 /**
