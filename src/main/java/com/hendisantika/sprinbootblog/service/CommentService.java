45fc72808bb4bdf80ec3632fa0dc1987b1f54661
hendisantika <hendisantika@yahoo.co.id>
2019-04-12 05:30:17 +0700

    #36 - Add CommentService

---
+++ src/main/java/com/hendisantika/sprinbootblog/service/CommentService.java
@@ -0,0 +1,17 @@
+package com.hendisantika.sprinbootblog.service;
+
+import com.hendisantika.sprinbootblog.model.Comment;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sprinboot-blog
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-12
+ * Time: 05:29
+ */
+public interface CommentService {
+
+    Comment save(Comment comment);
+}
