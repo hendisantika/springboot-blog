732fa4aaedc9d0d291a46cd1f801b3269467863c
hendisantika <hendisantika@yahoo.co.id>
2019-04-13 11:39:19 +0700

    #45 - Modify import-h2.sql file

--- src/main/resources/sql/import-h2.sql
+++ src/main/resources/sql/import-h2.sql
@@ -5,15 +5,15 @@ ALTER TABLE POST
 -- password in plaintext: "password"
 INSERT INTO USER (user_id, password, email, username, name, last_name, active)
 VALUES
-  (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'user@mail.com', 'user', 'Name', 'Surname',
+  (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'uzumaki_naruto@konohagakure.com', 'naruto', 'Uzumaki', 'Naruto',
    1);
 -- password in plaintext: "password"
 INSERT INTO USER (user_id, password, email, username, name, last_name, active)
 VALUES
-  (2, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'johndoe@gmail.com', 'johndoe', 'John', 'Doe', 1);
+  (2, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'uchiha_sasuke@konohagakure.com', 'sasuke', 'Uchiha', 'Sasuke', 1);
 -- password in plaintext: "password"
 INSERT INTO USER (user_id, password, email, username, name, last_name, active)
-VALUES (3, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'ana@mail.com', 'ana', 'Ana', 'Surname', 1);
+VALUES (3, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'haruno_sakura@konohagakure.com', 'sakura', 'Haruno', 'Sakura', 1);
 
 -- Roles
 INSERT INTO ROLE (role_id, role)
