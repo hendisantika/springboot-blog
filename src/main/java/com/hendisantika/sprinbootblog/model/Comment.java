818a979fb3a3e4fc0534360e544444d0628ed269
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-04-13 10:28:07 +0700

    Fix auto-generate ID on hibernate 5.2 (GenerationStrategy.AUTO not work)

--- src/main/java/com/hendisantika/sprinbootblog/model/Comment.java
+++ src/main/java/com/hendisantika/sprinbootblog/model/Comment.java
@@ -21,7 +21,7 @@ import java.util.Date;
 public class Comment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
     private Long id;
 
