d0fa15f217cbba37eb8fd0f6e3ffecb343a8fd11
hendisantika <hendisantika@yahoo.co.id>
2019-04-15 06:15:22 +0700

    #54 - Add editPostWithId & isPrincipalOwnerOfPost method in PostController

--- src/main/java/com/hendisantika/sprinbootblog/controller/PostController.java
+++ src/main/java/com/hendisantika/sprinbootblog/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -66,4 +67,30 @@ public class PostController {
         }
     }
 
+    @GetMapping("/editPost/{id}")
+    public String editPostWithId(@PathVariable Long id,
+                                 Principal principal,
+                                 Model model) {
+
+        Optional<Post> optionalPost = postService.findForId(id);
+
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+
+            if (isPrincipalOwnerOfPost(principal, post)) {
+                model.addAttribute("post", post);
+                return "/postForm";
+            } else {
+                return "/403";
+            }
+
+        } else {
+            return "/error";
+        }
+    }
+
+
+    private boolean isPrincipalOwnerOfPost(Principal principal, Post post) {
+        return principal != null && principal.getName().equals(post.getUser().getUsername());
+    }
 }
