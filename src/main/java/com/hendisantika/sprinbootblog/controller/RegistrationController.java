47b68993ad66a2c6b940de3e5d19761f44b15d61
hendisantika <hendisantika@yahoo.co.id>
2019-04-15 06:17:58 +0700

    #59 - Add createNewUser method in RegistrationController

--- src/main/java/com/hendisantika/sprinbootblog/controller/RegistrationController.java
+++ src/main/java/com/hendisantika/sprinbootblog/controller/RegistrationController.java
@@ -5,7 +5,11 @@ import com.hendisantika.sprinbootblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,6 +34,34 @@ public class RegistrationController {
     public String registration(Model model) {
 
         model.addAttribute("user", new User());
+        return "/registration";
+    }
+
+    @PostMapping("/registration")
+    public String createNewUser(@Valid User user,
+                                BindingResult bindingResult,
+                                Model model) {
+
+        if (userService.findByEmail(user.getEmail()).isPresent()) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the email provided");
+        }
+        if (userService.findByUsername(user.getUsername()).isPresent()) {
+            bindingResult
+                    .rejectValue("username", "error.user",
+                            "There is already a user registered with the username provided");
+        }
+
+        if (!bindingResult.hasErrors()) {
+            // Registration successful, save user
+            // Set user role to USER and set it as active
+            userService.save(user);
+
+            model.addAttribute("successMessage", "User has been registered successfully");
+            model.addAttribute("user", new User());
+        }
+
         return "/registration";
     }
 }
