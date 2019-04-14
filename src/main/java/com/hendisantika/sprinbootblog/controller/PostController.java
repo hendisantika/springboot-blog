cd6ae8235b24c036e5bb37a93a0fd2e654d8a220
hendisantika <hendisantika@yahoo.co.id>
2019-04-15 06:14:10 +0700

    #52 - Add newPost method in PostController

--- src/main/java/com/hendisantika/sprinbootblog/controller/PostController.java
+++ src/main/java/com/hendisantika/sprinbootblog/controller/PostController.java
@@ -1,9 +1,16 @@
 package com.hendisantika.sprinbootblog.controller;
 
+import com.hendisantika.sprinbootblog.model.Post;
+import com.hendisantika.sprinbootblog.model.User;
 import com.hendisantika.sprinbootblog.service.PostService;
 import com.hendisantika.sprinbootblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,6 +30,25 @@ public class PostController {
     public PostController(PostService postService, UserService userService) {
         this.postService = postService;
         this.userService = userService;
+    }
+
+    @GetMapping("/newPost")
+    public String newPost(Principal principal,
+                          Model model) {
+
+        Optional<User> user = userService.findByUsername(principal.getName());
+
+        if (user.isPresent()) {
+            Post post = new Post();
+            post.setUser(user.get());
+
+            model.addAttribute("post", post);
+
+            return "/postForm";
+
+        } else {
+            return "/error";
+        }
     }
 
 }
