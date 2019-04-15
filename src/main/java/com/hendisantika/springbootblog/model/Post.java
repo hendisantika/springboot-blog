50f47840326cec62f3b28e227a053a743cf6f6ae
hendisantika <hendisantika@yahoo.co.id>
2019-04-16 06:31:42 +0700

    #75 - Bug Fix Post Model

--- src/main/java/com/hendisantika/springbootblog/model/Post.java
+++ src/main/java/com/hendisantika/springbootblog/model/Post.java
@@ -23,7 +23,7 @@ import java.util.Date;
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private Long id;
 
